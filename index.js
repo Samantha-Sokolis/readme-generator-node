1cf8f6871eb1e45eb693ab64295f0778017476ae
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-10-13 20:48:26 +1100

    MIT license info added

--- index.js
+++ index.js
@@ -1,66 +1,65 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { generateMarkdown } = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([{
             type: 'input',
-            title: 'title',
+            name: 'title',
             message: 'What is the title of your project?',
         },
         {
             type: 'input',
-            description: 'description',
+            name: 'description',
             message: 'Write a description of your project',
         },
         {
             type: 'input',
-            installation: 'installation',
+            name: 'installation',
             message: 'Write your installation instructions',
         },
         {
             type: 'input',
-            usage: 'usage',
+            name: 'usage',
             message: 'Write your usage information',
         },
         {
             type: 'input',
-            contributing: 'contribution-guidelines',
+            name: 'contributing',
             message: 'What are your contribution guidelines?',
         },
         {
             type: 'input',
-            tests: 'test-instructions',
+            name: 'tests',
             message: 'What are your Test Instructions?',
         },
         {
             type: 'checkbox',
+            name: 'license',
             message: 'Choose a license',
-            license: 'license',
             choices: ['MIT', 'license 2', 'license 3', 'license 4'],
         },
         {
             type: 'input',
-            username: 'username',
+            name: 'username',
             message: 'Enter your GitHub username',
         },
         {
             type: 'input',
-            questions: 'questions',
+            name: 'questions',
             message: 'Enter your email address',
         }])
-
-    .then((data) => {
-        return input;
-    })
+        .then(input => {
+            return input;
+        })
 };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeMarkdown(fileName, data, err => {
+    fs.writeFile(fileName, data, err => {
         if(err) throw err;
         console.log('Your README has been generated')
     })
